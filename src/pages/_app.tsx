a7a559ec16a04588a9034f74ec590585aba1f932
Yousuf Basir <gourabxz@gmail.com>
2026-03-05 20:48:38 +0600

    add fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { EB_Garamond, Hanken_Grotesk } from "next/font/google";
+
+const ebGaramond = EB_Garamond({
+  subsets: ["latin"],
+  variable: "--font-eb-garamond",
+  display: "swap",
+});
+
+const hankenGrotesk = Hanken_Grotesk({
+  subsets: ["latin"],
+  variable: "--font-hanken-grotesk",
+  display: "swap",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={`${ebGaramond.variable} ${hankenGrotesk.variable} font-sans`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
